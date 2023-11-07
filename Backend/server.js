887477d6b085f7950f9d3272e667c9e73f39e870
hemanthkk <hemanthkkottila@gmail.com>
2023-11-07 18:24:32 +0530

    hai

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,82 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require("./config/database")
+const bcrypt = require('bcrypt');
+const db = require('./config/database');
+const cors=require('cors');
 
-// Create an Express application
 const app = express();
+const port = process.env.PORT || 5000;
 
-// Middleware
-app.use(bodyParser.json()); // Parse JSON request bodies
+app.use(bodyParser.json());
 
-// Define a sample route
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('Hello, Node.js!');
 });
 
-// Start the server
-const port = process.env.PORT || 5000;
+app.post('/register', (req, res) => {
+  // Parse the request body to retrieve 'name' and 'pass'
+  console.log('this is register page');
+  const { name, pass } = req.body;
+  
+  const saltRounds = 10;
+
+  // Hash the password
+  bcrypt.hash(pass, saltRounds, (err, hash) => {
+    if (err) {
+      console.error('Error in password hashing:', err);
+      res.status(500).send('Error in password hashing');
+    } else {
+      const newUser = {
+        user_name: name,
+        password: hash,
+        // Add other user data as needed
+      };
+      
+      // Insert the user into the database
+      db.query('INSERT INTO users SET ?', newUser, (dbErr, result) => {
+        if (dbErr) {
+          console.error('Error inserting data:', dbErr);
+          res.status(500).send('Error inserting data');
+        } else {
+          console.log('Data inserted:', result);
+          res.status(200).send('Data inserted successfully');
+        }
+      });
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  // Parse the request body to retrieve 'name' and 'pass'
+  const { username, password } = req.body;
+console.log('username: ', username,'password:',password);
+  // Retrieve the hashed password from your MySQL database (use your database query)
+  db.query('SELECT password FROM users WHERE user_name = ?', [username], (dbErr, dbResult) => {
+    if (dbErr) {
+      console.error('Error querying database:', dbErr);
+      res.status(500).send('Error querying database');
+    } else if (dbResult.length === 0) {
+      res.status(401).send('User not found');
+    } else {
+      const hashedPasswordFromDatabase = dbResult[0].password;
+      
+      // Compare the entered password with the stored hash
+      bcrypt.compare(password, hashedPasswordFromDatabase, (bcryptErr, result) => {
+        if (bcryptErr) {
+          console.error('Error comparing passwords:', bcryptErr);
+          res.status(500).send('Error comparing passwords');
+        } else if (result === true) {
+          res.status(200).send('Login successful');
+        } else {
+          res.status(401).send('Login failed');
+        }
+      });
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
---
+++ frontend/src/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [username, setusername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+       // console.log(email);
+        try {
+            console.log('response recived:' ,username,password);
+            const response = await fetch('http://localhost:5000/login', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ username, password }),
+            });
+            // Handle the response from your server
+          } catch (error) {
+            console.error('Error:', error);
+          }
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="username">User Name</label>
+                <input value={username} onChange={(e) => setusername(e.target.value)}type="text" placeholder="your username" id="username" name="username" />
+                <label htmlFor="password">Password</label>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
